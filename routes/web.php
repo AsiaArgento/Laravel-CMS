ff4104fe931006a265360d3bf0ab898bb446d97a
Matt M Moncada <misakamikotocsr@gmail.com>
2019-08-16 13:22:49 -0600

    resources and controllers

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/about', function () {
+//     return "This is the about page, sons.";
+// });
+//
+// Route::get('/contact', function () {
+//     return "This is the contact page, sons.";
+// });
+//
+// Route::get('/post/{id}/{name}', function($id, $name) {
+//     return "This is post number " . $id . " " . $name;
+// });
+//
+// Route::get('admin/posts/example', array('as'=>'admin.home' ,function() {
+//     $url = route('admin.home'); // grabbing this route and saving it in $url variable
+//     return "This url is " . $url;
+// })); // What this means is 'admin.home' is the same as 'admin/posts/example'
+
+// Route::get('/post/{id}', 'PostsController@index'); // when it goes to PostsController it will look for index method
+
+Route::resource('posts', 'PostsController');

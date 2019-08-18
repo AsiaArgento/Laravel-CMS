938398733f27a9a6227f61c039c762765954ddd2
Matt M Moncada <misakamikotocsr@gmail.com>
2019-08-18 16:44:39 -0600

    one to many relationship

--- routes/web.php
+++ routes/web.php
@@ -134,11 +134,19 @@ Route::get('/', function () {
 |--------------------------------------------------------------------------
 */
 
-// One to One Relationship
-Route::get('/user/{id}/post', function($id){
-  return User::find($id)->post->title;
-});
+// One to One Relationship ***************
+// Route::get('/user/{id}/post', function($id){
+//   return User::find($id)->post->title;
+// });
+//
+// Route::get('/post/{id}/user', function($id){
+//   return Post::find($id)->user->name;
+// });
 
-Route::get('/post/{id}/user', function($id){
-  return Post::find($id)->user->name;
+// One to Many Relationship *****************
+Route::get('/posts', function(){
+  $user = User::find(1);
+  foreach($user->posts as $post){
+    echo $post->title . "<br>";
+  }
 });
